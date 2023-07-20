b205999e187365f8dab718c2cf4aaf96d23992ba
Ana Clara Tomaselli Borchardt <130708738+anaclaraborchardt@users.noreply.github.com>
2023-07-20 12:30:52 -0300

    Define ganhador

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Scanner sc = new Scanner(System.in);
-    static Jogador jogador = new Jogador();
-    static int jogadorAtual = 1;
-
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         Campo campo = new Campo();
-
-
-        Infantaria infantaria = new Infantaria();
+        Jogador jogador = new Jogador();
+        int jogadorAtual = 1;
         Cavalaria cavalaria = new Cavalaria();
+        Infantaria infantaria = new Infantaria();
         Arqueiros arqueiros = new Arqueiros();
 
         do {
+            System.out.println("Jogador " + jogadorAtual);
+            System.out.println(jogadorAtual == 1 ? jogador.unidadesJogador1 : jogador.unidadesJogador2);
+            campo.imprimirJogo();
 
-            Integer posicaoOrigem = null;
-            do{
-                campo.imprimirJogo();
-                defineGanhador();
-                System.out.println(jogador.unidadesJogador1);
-                posicaoOrigem = mover();
-            }while(posicaoOrigem == null);
+            boolean escolhaValida = false;
+            int posicaoOrigem;
 
-                    System.out.println("Para qual posição você deseja ir? (Digite o índice de destino)");
-                    System.out.println("\nCaso queira atacar, digite 100");
-                    int posicaoDestino = sc.nextInt();
+            do {
+                System.out.println("Qual posição deseja mover? (Digite o índice da peça).");
+                posicaoOrigem = sc.nextInt();
+
+                ArrayList<Integer> validIndices = jogador.pegaIndice(jogadorAtual);
+                if (!validIndices.contains(posicaoOrigem)) {
+                    System.out.println("Essa não é uma de suas peças");
+                    System.out.println("Escolha outra posição: ");
+                } else {
+                    escolhaValida = true;
+                }
+            } while (!escolhaValida);
+
+            System.out.println("Para qual posição você deseja ir? (Digite o índice de destino)");
+            System.out.println("\nCaso queira atacar, digite 100");
+            int posicaoDestino = sc.nextInt();
 
                     if (posicaoDestino == 100) {
                         System.out.println("Qual a peca que deseja atacar 1- Infant; 2- Caval");
@@ -67,40 +74,33 @@ public class Main {
                             cavalaria.mover(campo, posicaoOrigemObj, posicaoDestino);
                             infantaria.mover(campo, posicaoOrigemObj, posicaoDestino);
                             arqueiros.mover(campo, posicaoOrigemObj, posicaoDestino);
+                            jogador.atualizaIndices(jogadorAtual, posicaoOrigem, posicaoDestino);
                         } else {
                             System.out.println("A posição de origem é inválida.");
                         }
                     }
-
+                    if (jogadorAtual == 1) {
+                        jogadorAtual = 2;
+                        jogador.setUnidades(jogador.unidadesJogador2);
+                    } else if (jogadorAtual == 2) {
+                        jogadorAtual = 1;
+                        jogador.setUnidades(jogador.unidadesJogador1);
+                    }
 
 
 
         } while (true);
     }
-
-    public static Integer mover() {
-
-        System.out.println("Arqueiro: anda 1 ou 2 espaços para frente ou um para o lado;\n" +
-                "Cavalaria: Pode andar para frente, trás, lado esquerdo ou direito\n" +
-                "Arqueiro não pode se mover\n" +
-                "O objetivo final é atacar o castelo");
-
-        System.out.println("\nQual posição deseja mover ou atacar? (Digite o índice da peça).");
-        int posicaoOrigem = sc.nextInt();
-
-        if (jogadorAtual == 1) {
-            jogadorAtual = 2;
-            jogador.setUnidades(jogador.unidadesJogador2);
-        } else if (jogadorAtual == 2) {
-            jogadorAtual = 1;
-            jogador.setUnidades(jogador.unidadesJogador1);
-        }
-        return posicaoOrigem;
-    }
-
-    public static void defineGanhador(){
-        if(jogador.ganhar() == true){
-            System.out.println("O ganhador é o jogador "+ jogadorAtual);
+    public static void confereVencedor(Jogador jogador1, Jogador jogador2) {
+        if (!jogador1.aindaHaPecas(1) && !jogador2.aindaHaPecas(2)) {
+            System.out.println("Empate! Ambos os jogadores ficaram sem peças.");
+        } else if (!jogador1.aindaHaPecas(1)) {
+            System.out.println("Jogador 2 venceu! Jogador 1 ficou sem peças.");
+        } else if (!jogador2.aindaHaPecas(2)) {
+            System.out.println("Jogador 1 venceu! Jogador 2 ficou sem peças.");
+        } else {
+            System.out.println("O jogo ainda não terminou. Ambos os jogadores têm peças.");
         }
     }
+
 }
