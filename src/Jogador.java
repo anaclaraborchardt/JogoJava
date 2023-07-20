98b5a85b40f10e973718efb46f35083273ad6b0d
Ana Clara Tomaselli Borchardt <130708738+anaclaraborchardt@users.noreply.github.com>
2023-07-20 12:31:36 -0300

    define Ganhador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 public class Jogador {
 
     private int numero;
     ArrayList<Unidade> unidadesJogador1 = new ArrayList<>();
     ArrayList<Unidade> unidadesJogador2 = new ArrayList<>();
+    private ArrayList<Integer> indicesValidosJogador1 = new ArrayList<>();
+    private ArrayList<Integer> indicesValidosJogador2 = new ArrayList<>();
 
     Unidade cavalaria = new Cavalaria();
     Unidade infantaria = new Infantaria();
@@ -31,7 +35,15 @@ public class Jogador {
             unidadesJogador1.add(new Cavalaria());
             unidadesJogador2.add(new Cavalaria());
         }
+        for (int i = 0; i <= 23; i++) {
+            indicesValidosJogador1.add(i);
+        }
+
+        for (int i = 60; i <= 83; i++) {
+            indicesValidosJogador2.add(i);
+        }
     }
+
 
     public int getNumero() {
         return numero;
@@ -45,18 +57,18 @@ public class Jogador {
         this.unidadesJogador2 = unidadesJogador2;
     }
 
-    public boolean ganhar() {
-        for (Unidade unidade : unidadesJogador1 ) {
-            if (unidade instanceof Tesouro) {
-                return false;
-            }
-        }
-        for (Unidade unidade : unidadesJogador2) {
-            if (unidade instanceof Tesouro) {
-                return false;
-            }
-        }
-        return true;
+    public boolean aindaHaPecas(int jogadorAtual) {
+        ArrayList<Unidade> unidadesJogador = jogadorAtual == 1 ? unidadesJogador1 : unidadesJogador2;
+        return !unidadesJogador.isEmpty();
+    }
+
+    public ArrayList<Integer> pegaIndice(int jogadorAtual) {
+        return jogadorAtual == 1 ? indicesValidosJogador1 : indicesValidosJogador2;
+    }
+
+    public void atualizaIndices(int jogadorAtual, int posicaoOrigem, int posicaoDestino) {
+        ArrayList<Integer> validIndices = pegaIndice(jogadorAtual);
+        validIndices.remove(Integer.valueOf(posicaoOrigem));
+        validIndices.add(posicaoDestino);
     }
 }
-
